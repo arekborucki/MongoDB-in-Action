4397178e261f6e4453690fbc662a89053810f5d8
Arek Borucki <68437078+arekborucki@users.noreply.github.com>
2025-04-13 13:04:33 +0200

    Update updateOperations.js

--- Chapter-4/updateOperations.js
+++ Chapter-4/updateOperations.js
@@ -14,11 +14,60 @@ db.routes.updateOne(
 db.routes.updateOne(
   { "airline.id": 413, "src_airport": "DFW", "dst_airport": "LAX" },
   { $push: { prices: { class: "business", price: 2500 } } }
-);
+)
 
-// Update array with arrayFilters
+db.routes.updateOne(
+  { "airline.id": 413, "src_airport": "DFW", "dst_airport": "LAX" },
+  { $push: { prices: { $each: [{ class: "economy", price: 800 }, { class: "first", price: 2000 }] } } }
+)
+
+db.routes.updateOne(
+  { "airline.id": 413, "src_airport": "DFW", "dst_airport": "LAX" },
+  {
+    $push: {
+      prices: {
+        $each: [
+          { class: "premium economy", price: 1100 },
+          { class: "luxury", price: 3000 }
+        ],
+        $sort: { price: 1 },
+        $slice: -3
+      }
+    }
+  }
+)
+
+db.routes.updateOne(
+  { "airline.id": 413, "src_airport": "DFW", "dst_airport": "LAX" },
+  { $addToSet: { prices: { class: "economy plus", price: 1200 } } }
+)
+
+
+db.routes.updateOne(
+  { "airline.id": 413, "src_airport": "DFW", "dst_airport": "LAX" },
+  { $pull: { prices: { class: "first", price: 2000 } } }
+)
+
+db.routes.updateOne(
+  { "airline.id": 413, "src_airport": "DFW", "dst_airport": "LAX" },
+  { $pop: { prices: 1 } } // usuwa ostatni
+)
+
+
+db.routes.updateOne(
+  { "airline.id": 413, "src_airport": "DFW", "dst_airport": "LAX" },
+  { $set: { "prices.2.price": 950 } }
+)
+
+
+db.routes.updateOne(
+  { "airline.id": 413, "src_airport": "DFW", "dst_airport": "LAX", "prices.class": "luxury" },
+  { $set: { "prices.$.price": 3500 } }
+)
+
 db.routes.updateOne(
   { "airline.id": 413, "src_airport": "DFW", "dst_airport": "LAX" },
   { $set: { "prices.$[elem].price": 600 } },
-  { arrayFilters: [ { "elem.class": "economy" } ] }
+  { arrayFilters: [{ "elem.class": "economy" }] }
 )
+
