87224b1909007c01dfe0d17c5ae7e7452cc4bd65
Arek Borucki <68437078+arekborucki@users.noreply.github.com>
2025-04-13 12:26:55 +0200

    Update connect.sh

--- Chapter-2/connect.sh
+++ Chapter-2/connect.sh
@@ -2,9 +2,8 @@
 
 # Connect to MongoDB Atlas cluster using mongosh
 # Replace <your-cluster-url> and <your-username> with actual values
+# Tip: Get the connection string from `atlas clusters connectionStrings describe <cluster-name>`
 
 mongosh "mongodb+srv://<your-cluster-url>.mongodb.net" \
   --apiVersion 1 \
   --username "<your-username>"
-
-
