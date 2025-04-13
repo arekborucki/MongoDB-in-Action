a0bedce686e8c5439c11f89aa6c4acf90467e32e
Arek Borucki <68437078+arekborucki@users.noreply.github.com>
2025-04-13 13:08:46 +0200

    Update deleteOperations.js

--- Chapter-4/deleteOperations.js
+++ Chapter-4/deleteOperations.js
@@ -1,5 +1,13 @@
-// Delete one document
-db.routes.deleteOne({ "airline.id": 413, "src_airport": "DFW", "dst_airport": "LAX" })
+// Delete a single document matching the given route
+db.routes.deleteOne({
+  "airline.id": 417,
+  "src_airport": "MUC",
+  "dst_airport": "LAX"
+})
 
-// Delete many documents
-db.routes.deleteMany({ "src_airport": "CDG" })
+// Delete a single document matching the given route
+db.routes.deleteOne({
+  "airline.id": 417,
+  "src_airport": "MUC",
+  "dst_airport": "LAX"
+})
