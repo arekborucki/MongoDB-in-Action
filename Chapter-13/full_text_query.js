89d5bdc4438f36f388dd8bafd85752b830908d2f
Arek Borucki <68437078+arekborucki@users.noreply.github.com>
2025-04-13 17:58:37 +0200

    Update full_text_query.js

--- Chapter-13/full_text_query.js
+++ Chapter-13/full_text_query.js
@@ -3,26 +3,47 @@
 // Executes a full-text search query with fuzzy matching and category filter
 // using the Atlas Search $search stage in an aggregation pipeline.
 
-db.movies.aggregate([
+db.inspections.aggregate([
   {
     $search: {
-      index: "default",
+      index: 'LocalSearchIndex',
       text: {
-        query: "time travel",
-        path: "plot",
+        query: 'No Violation Issued',
+        path: ['result', 'business_name'],
         fuzzy: {
-          maxEdits: 1
+          maxEdits: 2
         }
       }
     }
   },
   {
-    $match: { genres: "Sci-Fi" }
+    $match: {
+      sector: 'Cigarette Retail Dealer - 127',
+      'address.city': 'RIDGEWOOD'
+    }
+  },
+  {
+    $addFields: {
+      score: { $meta: "searchScore" }
+    }
+  },
+  {
+    $sort: { score: -1 }
+  },
+  {
+    $limit: 3
   },
   {
     $project: {
-      title: 1,
-      score: { $meta: "searchScore" }
+      _id: 0,
+      business_name: 1,
+      certificate_number: 1,
+      date: 1,
+      result: 1,
+      'address.street': 1,
+      'address.number': 1,
+      'address.zip': 1,
+      score: 1
     }
   }
 ])
