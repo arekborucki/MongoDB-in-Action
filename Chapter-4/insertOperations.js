f9420ad8a5ef74d2a02b48fdb9b404a76f0f3fa1
Arek Borucki <68437078+arekborucki@users.noreply.github.com>
2025-04-13 12:47:32 +0200

    Create insertOperations.js

---
+++ Chapter-4/insertOperations.js
@@ -0,0 +1,25 @@
+// Inserts one and many documents into sample_training.routes
+
+db.routes.insertOne({
+  airline: { id: 410, name: 'Lufthansa', alias: 'LH', iata: 'DLH' },
+  src_airport: 'MUC',
+  dst_airport: 'JFK',
+  codeshare: '',
+  stops: 0,
+  airplane: 'A380'
+})
+
+db.routes.insertMany([
+  {
+    airline: { id: 413, name: 'American Airlines', alias: 'AA', iata: 'AAL' },
+    src_airport: 'DFW', dst_airport: 'LAX', codeshare: '', stops: 0, airplane: '737'
+  },
+  {
+    airline: { id: 411, name: 'British Airways', alias: 'BA', iata: 'BAW' },
+    src_airport: 'LHR', dst_airport: 'SFO', codeshare: 'Y', stops: 0, airplane: '747'
+  },
+  {
+    airline: { id: 412, name: 'Air France', alias: 'AF', iata: 'AFR' },
+    src_airport: 'CDG', dst_airport: 'JFK', codeshare: '', stops: 0, airplane: '777'
+  }
+])
